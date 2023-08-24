eca6d5b85b4a497c33ee6b78708069691de84584
Evhen <malysh.evgeniy@gmail.com>
2023-08-24 19:12:27 +0300

    initial commit

---
+++ src/test/java/baza/trainee/KavaleridzeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package baza.trainee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KavaleridzeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
