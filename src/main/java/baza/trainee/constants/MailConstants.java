3f7e498e165f4464df80625f4c97371b47611d20
Anatolii_Omelchenko <omelchenko20@gmail.com>
2023-09-09 17:42:26 +0300

    chore: added MailDtoTest

--- src/main/java/baza/trainee/constants/MailConstants.java
+++ src/main/java/baza/trainee/constants/MailConstants.java
@@ -10,8 +10,15 @@ public final class MailConstants {
     public static final String FAIL_SEND_MSG = "failed to send email";
     public static final String MUSEUM_TEMPLATE_PATH = "templates/msg_for_museum.html";
     public static final String USER_TEMPLATE_PATH = "templates/msg_for_user.html";
+
     public static final int MIN_NAME_LENGTH = 2;
     public static final int MAX_NAME_LENGTH = 30;
     public static final int MIN_MSG_LENGTH = 10;
     public static final int MAX_MSG_LENGTH = 300;
+    public static final String FIELD_NAME_ERROR_MSG =
+            "Name should be between " + MIN_NAME_LENGTH + " and " + MAX_NAME_LENGTH + " characters";
+    public static final String FIELD_EMAIL_ERROR_MSG = "Invalid email";
+    public static final String FIELD_MESSAGE_ERROR_MSG =
+            "Message should be between " + MIN_MSG_LENGTH + " and " + MAX_MSG_LENGTH + " characters";
+    public static final String NULL_FIELD_ERROR_MSG = "Filed cant be empty";
 }
--- src/main/java/baza/trainee/domain/dto/MailDto.java
+++ src/main/java/baza/trainee/domain/dto/MailDto.java
@@ -4,26 +4,30 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import static baza.trainee.constants.MailConstants.FIELD_EMAIL_ERROR_MSG;
+import static baza.trainee.constants.MailConstants.FIELD_MESSAGE_ERROR_MSG;
+import static baza.trainee.constants.MailConstants.FIELD_NAME_ERROR_MSG;
 import static baza.trainee.constants.MailConstants.MAX_MSG_LENGTH;
 import static baza.trainee.constants.MailConstants.MAX_NAME_LENGTH;
 import static baza.trainee.constants.MailConstants.MIN_MSG_LENGTH;
 import static baza.trainee.constants.MailConstants.MIN_NAME_LENGTH;
+import static baza.trainee.constants.MailConstants.NULL_FIELD_ERROR_MSG;
 
 public record MailDto(
-        @NotBlank
-        @Size(min = MIN_NAME_LENGTH, max = MAX_NAME_LENGTH)
+        @NotBlank(message = NULL_FIELD_ERROR_MSG)
+        @Size(min = MIN_NAME_LENGTH, max = MAX_NAME_LENGTH, message = FIELD_NAME_ERROR_MSG)
         String firstName,
 
-        @NotBlank
-        @Size(min = MIN_NAME_LENGTH, max = MAX_NAME_LENGTH)
+        @NotBlank(message = NULL_FIELD_ERROR_MSG)
+        @Size(min = MIN_NAME_LENGTH, max = MAX_NAME_LENGTH, message = FIELD_NAME_ERROR_MSG)
         String lastName,
 
-        @NotBlank
-        @Email
+        @NotBlank(message = NULL_FIELD_ERROR_MSG)
+        @Email(message = FIELD_EMAIL_ERROR_MSG)
         String email,
 
-        @NotBlank
-        @Size(min = MIN_MSG_LENGTH, max = MAX_MSG_LENGTH)
+        @NotBlank(message = NULL_FIELD_ERROR_MSG)
+        @Size(min = MIN_MSG_LENGTH, max = MAX_MSG_LENGTH, message = FIELD_MESSAGE_ERROR_MSG)
         String message
 ) {
 }
