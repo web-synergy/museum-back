499f312ee6eeed66acd236a6b4e58dc6b22d7908
Omelchenko Anatolii <108679243+Scream274@users.noreply.github.com>
2023-09-10 17:45:32 +0300

    Feature/create mail service (#9)
    * feat: added MailService, dependencies, changed application.yml
    
    * feat: added MailController, MailDto, html-templates.
    
    * feat: added EmailSendingException
    
    * feat: added MailConstants file and validation fot MailDto
    
    * chore: added MailControllerTest
    
    * chore: added MailServiceTest
    
    * chore: added MailDtoTest
    
    * refactor: fixed code according reviews comments.

---
+++ src/main/java/baza/trainee/exceptions/custom/EmailSendingException.java
@@ -0,0 +1,15 @@
+package baza.trainee.exceptions.custom;
+
+import org.springframework.http.HttpStatus;
+
+public class EmailSendingException extends BasicApplicationException {
+
+    /**
+     * Constructs EmailSendingException with the specified error message and HTTP status.
+     *
+     * @param message Error message explaining the reason for the exception.
+     */
+    public EmailSendingException(final String message) {
+        super(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ src/main/java/baza/trainee/service/MailService.java
@@ -0,0 +1,34 @@
+package baza.trainee.service;
+
+
+public interface MailService {
+
+    /**
+     * Sends an email from the specified email address.
+     *
+     * @param to      The email address of the recipient.
+     * @param message The message text to be sent in the email.
+     * @param subject The subject of the email.
+     */
+    void sendEmail(String to, String message, String subject);
+
+    /**
+     * Builds a message for a user by combining their first name and last name.
+     *
+     * @param firstName The first name of the user.
+     * @param lastName  The last name of the user.
+     * @return A formatted html-message string that includes the user's name.
+     */
+    String buildMsgForUser(String firstName, String lastName);
+
+    /**
+     * Builds a message for a museum, including user information and a custom message.
+     *
+     * @param firstName The first name of the user.
+     * @param lastName  The last name of the user.
+     * @param email     The email address of the user.
+     * @param message   The message from user.
+     * @return A formatted html-message string for the museum that includes user information and message.
+     */
+    String buildMsgForMuseum(String firstName, String lastName, String email, String message);
+}
