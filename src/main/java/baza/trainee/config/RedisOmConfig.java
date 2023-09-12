33497535ac030a7e076b0b928b1f8013f55315ad
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-12 10:29:31 +0300

    fix: add redis document sources to config

--- src/main/java/baza/trainee/config/RedisOmConfig.java
+++ src/main/java/baza/trainee/config/RedisOmConfig.java
@@ -1,9 +1,11 @@
 package baza.trainee.config;
 
+import baza.trainee.domain.model.Event;
+import baza.trainee.repository.EventRepository;
 import com.redis.om.spring.annotations.EnableRedisDocumentRepositories;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableRedisDocumentRepositories
+@EnableRedisDocumentRepositories(basePackageClasses = { EventRepository.class, Event.class })
 public class RedisOmConfig {
 }
