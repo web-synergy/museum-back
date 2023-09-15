7e5309f84bd6609f06676da2c131da1104ba5898
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-15 19:37:18 +0300

    refactor: add basePackages to Redis Scan

--- src/main/java/baza/trainee/config/RedisOmConfig.java
+++ src/main/java/baza/trainee/config/RedisOmConfig.java
@@ -1,22 +1,9 @@
 package baza.trainee.config;
 
 import com.redis.om.spring.annotations.EnableRedisDocumentRepositories;
-
-import baza.trainee.domain.model.Article;
-import baza.trainee.domain.model.Event;
-import baza.trainee.domain.model.ContentBlock;
-import baza.trainee.repository.ArticleRepository;
-import baza.trainee.repository.EventRepository;
-
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableRedisDocumentRepositories(basePackageClasses = {
-        EventRepository.class,
-        ArticleRepository.class,
-        Event.class,
-        Article.class,
-        ContentBlock.class
-})
+@EnableRedisDocumentRepositories(basePackages = "baza.trainee.**")
 public class RedisOmConfig {
 }
