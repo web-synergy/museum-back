88f308c0f188c9d19be499e1826fba5cfa912550
Andry Sitarskiy <andreysitarskiy09@gmail.com>
2023-10-12 17:36:58 +0300

    Merge remote-tracking branch 'origin/update_login_admin_v2' into update_login_admin_v2

--- src/main/java/baza/trainee/service/impl/AdminLoginServiceImpl.java
+++ src/main/java/baza/trainee/service/impl/AdminLoginServiceImpl.java
@@ -9,22 +9,27 @@ import baza.trainee.service.MailService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static baza.trainee.constants.MailConstants.ACTIVATION_COD;
 
 @Service
 @RequiredArgsConstructor
 public class AdminLoginServiceImpl implements AdminLoginService {
-    private static final String OLD_LOGIN = "oldLogin";
-    private static final String NEW_LOGIN = "newLogin";
-    private static final String CODE_CHANGE = "codChange";
-    private static final int SIX_DIGIT_CODE = 999999;
+
+    private static final String OLD_LOGIN_KEY = "oldLogin";
+    private static final String NEW_LOGIN_KEY = "newLogin";
+    private static final String VERIFICATION_CODE_KEY = "codChange";
+    private static final int MAX_SIX_DIGIT_CODE = 999999;
+
     private final MailService mailService;
     private final UserRepository userRepository;
-    private final Random random = new Random();
+    private final RedisTemplate<String, String> template;
+
 
     /**
      * Check login.
@@ -62,32 +67,33 @@ public class AdminLoginServiceImpl implements AdminLoginService {
         checkDuplicateLoginInBase(loginDto.getNewLogin());
         String code = createCodeChange();
         sendEmail(code, loginDto.getNewLogin());
-        saveSettingLogin(loginDto, code, session);
+        saveSettingLogin(loginDto, code);
     }
 
-    private void checkDuplicateLoginInBase(final String login) {
-        if (userRepository.findByEmail(login).isEmpty()) {
+    private void checkDuplicateLoginInBase(final String username) {
+        if (userRepository.findByEmail(username).isPresent()) {
             throw new LoginNotValidException("This user already exists in the database");
         }
     }
 
     @Override
-    public void changeLogin(String cod, HttpSession session) {
-        if (!cod.equals(session.getAttribute(CODE_CHANGE))) {
-            throw new LoginNotValidException("Not valid cod");
+    public void changeLogin(String code, HttpSession session) {
+        if (!code.equals(session.getAttribute(VERIFICATION_CODE_KEY))) {
+            throw new LoginNotValidException("Not valid code");
         }
         User admin = userRepository.findByEmail(
-                (String) session.getAttribute(OLD_LOGIN))
+                (String) session.getAttribute(OLD_LOGIN_KEY))
                 .orElseThrow(() -> new LoginNotValidException("Not find user by login"));
-        admin.setEmail((String) session.getAttribute(NEW_LOGIN));
+        admin.setEmail((String) session.getAttribute(NEW_LOGIN_KEY));
         userRepository.save(admin);
 
     }
 
-    private void saveSettingLogin(LoginDto loginDto, String code, HttpSession session) {
-        session.setAttribute(OLD_LOGIN, loginDto.getOldLogin());
-        session.setAttribute(NEW_LOGIN, loginDto.getNewLogin());
-        session.setAttribute(CODE_CHANGE, code);
+    private void saveSettingLogin(LoginDto loginDto, String code) {
+       /* template.
+        session.setAttribute(OLD_LOGIN_KEY, loginDto.getOldLogin());
+        session.setAttribute(NEW_LOGIN_KEY, loginDto.getNewLogin());
+        session.setAttribute(VERIFICATION_CODE_KEY, code);*/
     }
 
     private void sendEmail(final String code, @Email String email) {
@@ -96,7 +102,7 @@ public class AdminLoginServiceImpl implements AdminLoginService {
     }
 
     private String createCodeChange() {
-        int randomNumber = random.nextInt(SIX_DIGIT_CODE);
+        int randomNumber = ThreadLocalRandom.current().nextInt(MAX_SIX_DIGIT_CODE);
         return String.format("%06d", randomNumber);
     }
 }
