84cb3cc98375838923bffbf210c079bd939c70fb
Evhen <malysh.evgeniy@gmail.com>
2023-10-25 11:42:20 +0300

    style: remove unused import

--- src/main/java/baza/trainee/api/impl/ImageApiDelegateImpl.java
+++ src/main/java/baza/trainee/api/impl/ImageApiDelegateImpl.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
